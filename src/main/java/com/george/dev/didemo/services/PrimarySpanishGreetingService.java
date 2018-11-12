8abcdf6fe91db7d5e9f0642c3335f5e67891633d
george <gokereka@teamapt.com>
2018-11-12 16:18:20 +0100

    Added a new service greeting service

---
+++ src/main/java/com/george/dev/didemo/services/PrimarySpanishGreetingService.java
@@ -0,0 +1,8 @@
+package com.george.dev.didemo.services;
+
+public class PrimarySpanishGreetingService implements GreetingService {
+    @Override
+    public String sayGreeting(){
+        return "Hello - Primary Gretting service";
+    }
+}
